d882b119496052a346a24cf82fbffe504cd9bf56
tien96ng <tien96ng@gmail.com>
2021-04-23 11:42:33 -0700

    Create a kegData.js to use as mock data to render the app.

---
+++ src/data/kegData.js
@@ -0,0 +1,28 @@
+const kegStorage = [
+  {
+    name: "MODELO ESPECIAL",
+    brand: "Modelo",
+    price: "6",
+    alcoholContent: "4.4",
+    flavor: "Lemon",
+    remainingPints: 124
+  },
+  {
+    name: "SHOCK TOP BELGIAN WHITE ALE",
+    brand: "Shock Top",
+    price: "3",
+    alcoholContent: "3",
+    flavor: "Orange",
+    remainingPints: 124
+  },
+  {
+    name: "ALASKAN AMBER",
+    brand: "Alaskan",
+    price: "4",
+    alcoholContent: "4.2",
+    flavor: "Caramel",
+    remainingPints: 124
+  }
+];
+
+export default kegStorage;
