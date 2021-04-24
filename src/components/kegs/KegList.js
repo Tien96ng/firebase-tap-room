022ebb6fc62975129223f1f2b2e8a9139a164c1b
tien96ng <tien96ng@gmail.com>
2021-04-23 17:34:19 -0700

    Add proper captilization to newKeg information

--- src/components/kegs/KegList.js
+++ src/components/kegs/KegList.js
@@ -49,6 +49,8 @@ export default class KegList extends Component {
     e.preventDefault();
     let tempKeg = this.state.newKeg;
     tempKeg.name = tempKeg.name.toUpperCase();
+    tempKeg.brand = tempKeg.name.slice(0, 1).toUpperCase() + tempKeg.name.slice(1).toLowerCase();
+    tempKeg.flavor = tempKeg.name.slice(0, 1).toUpperCase() + tempKeg.name.slice(1).toLowerCase();
 
     let emptyKeg = {
       name: "",
