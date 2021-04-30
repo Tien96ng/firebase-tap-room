7ab235d350e6666c76fea861755cbb1f2a373db0
tien96ng <tien96ng@gmail.com>
2021-04-30 13:02:43 -0700

    Create ActionTypes.js for Action constants and actionCreators

---
+++ src/components/redux/actions/ActionTypes.js
@@ -0,0 +1,11 @@
+export const KEG_LIST = "KEG_LIST";
+export const SHOW_KEG_FORM = "SHOW_KEG_FORM";
+export const RESET_KEG_FORM = "RESET_KEG_FORM";
+export const SELL_KEG = "SELL_KEG";
+export const KEG_SUBMISSION = "KEG_SUBMISSION";
+
+
+export const handleFormChange = field => ({
+  type: "HANDLE_FORM_CHANGE",
+  payload: field
+});
